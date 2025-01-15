b00bef84fec2bfc22d5f2118baee0e924c252dc2
Kamronbek Atajanov <KAMRON-DEVELOPER>
2025-01-15 13:47:13 +0500

    again some changes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,67 +1,35 @@
 return {
-  {
-    "zaldih/themery.nvim",
-    lazy = false,
+	{
+		"zaldih/themery.nvim",
+		lazy = false,
+		dependencies = { "ellisonleao/gruvbox.nvim", "navarasu/onedark.nvim", "folke/tokyonight.nvim" },
 
-    config = function()
-      require("themery").setup({
-        themes = {
-          "onedark",
-          "catppuccin",
-          "tokyonight",
-          {
-            name = "Gruvbox dark",
-            colorscheme = "gruvbox",
-            before = [[vim.opt.background = "dark"]],
-          },
-          {
-            name = "Gruvbox light",
-            colorscheme = "gruvbox",
-            before = [[vim.opt.background = "light"]],
-          }
-        },
-        livePreview = true,
-      })
-    end
-  },
-  {
-    "navarasu/onedark.nvim",
-    lazy = false,
-    priority = 1000,
+		config = function()
+			require("themery").setup({
+				livePreview = true,
+				themes = {
+					"onedark",
+					"tokyonight",
+					{
+						name = "Gruvbox dark",
+						colorscheme = "gruvbox",
+						before = [[vim.opt.background = "dark"]],
+					},
+					{
+						name = "Gruvbox light",
+						colorscheme = "gruvbox",
+						before = [[vim.opt.background = "light"]],
+					},
+				},
+			})
 
-    -- config = function()
-    --   require("onedark").setup({
-    --     transparent = true,
-    --     style = "cool", -- "dark", "darker", "cool", "deep", "warm", "warmer"
-    --   })
-    --   vim.cmd("colorscheme onedark")
-    -- end,
-  },
-  {
-    {
-      "catppuccin/nvim",
-      lazy = false,
-      name = "catppuccin",
-      priority = 1000,
-
-      -- config = function()
-      --   vim.cmd("colorscheme catppuccin-frappe") -- latte, frappe, macchiato, mocha
-      -- end
-    }
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-
-    -- config = function()
-    --   vim.cmd("colorscheme tokyonight-storm") -- moon, storm, night, day
-    -- end,
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = true,
-    opts = {}
-  }
+			vim.keymap.set("n", "<leader>tc", ":Themery<CR>", { desc = "Themery" })
+		end,
+	},
+	{
+		"xiyaowong/transparent.nvim",
+		config = function()
+			vim.keymap.set("n", "<leader>tt", ":TransparentToggle<CR>", { desc = "Toggle transparent" })
+		end,
+	},
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,66 +4,68 @@
 -- +-------------------------------------------------+
 
 return {
-  "nvim-lualine/lualine.nvim",
+	"nvim-lualine/lualine.nvim",
 
-  config = function()
-    local lualine = require("lualine")
+	config = function()
+		local lualine = require("lualine")
 
-    lualine.setup({
-      options = {
-        -- theme = "auto",
-        -- theme = "iceberg_dark",
-        -- theme = "codedark",
-        -- theme = "base16",
-        -- theme = "tokyonight",
-        -- theme = "gruvbox_dark",
-        theme = "dracula",
-        -- theme = "ayu_mirage",
-        -- theme = "horizon",
-        -- theme = "nord",
-        -- theme = "onedark",
+		lualine.setup({
+			options = {
+				-- theme = "auto",
+				-- theme = "iceberg_dark",
+				-- theme = "codedark",
+				-- theme = "base16",
+				-- theme = "tokyonight",
+				-- theme = "gruvbox_dark",
+				-- theme = "jellybeans",
+				theme = "dracula",
+				-- theme = "ayu_mirage",
+				-- theme = "horizon",
+				-- theme = "nord",
+				-- theme = "onedark",
+				-- theme = "wombat",
 
-        -- component_separators = { left = '', right = '' }, -- beetween
-        -- section_separators = { left = "", right = "" }, -- inside
-        component_separators = { left = "", right = "" }, -- beetween
-        section_separators = { left = "", right = "" }, -- inside
-      },
-      disabled_filetypes = {
-        statusline = {},
-        winbar = {},
-      },
-      ignore_focus = {},
-      always_divide_middle = true,
-      globalstatus = false,
-      refresh = {
-        statusline = 1000,
-        tabline = 1000,
-        winbar = 1000,
-      },
-      sections = {
-        lualine_a = {
-          "mode",
-          -- { "mode", separator = { left = "", right = "" }, right_padding = 2 },
-        },
-        lualine_b = { "branch" },
-        lualine_c = {},
-        lualine_x = { "diagnostics", "filesize" },
-        lualine_y = { "filename" },
-        lualine_z = {
-          "location",
-          -- { "location", separator = { left = "", right = "" }, left_padding = 2 },
-        },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      extensions = {},
-    })
-  end,
+				-- component_separators = { left = '', right = '' }, -- beetween
+				-- section_separators = { left = "", right = "" }, -- inside
+				-- component_separators = { left = "", right = "" }, -- beetween
+				-- section_separators = { left = "", right = "" }, -- inside
+			},
+			disabled_filetypes = {
+				statusline = {},
+				winbar = {},
+			},
+			ignore_focus = {},
+			always_divide_middle = true,
+			globalstatus = false,
+			refresh = {
+				statusline = 1000,
+				tabline = 1000,
+				winbar = 1000,
+			},
+			sections = {
+				lualine_a = {
+					"mode",
+					-- { "mode", separator = { left = "", right = "" }, right_padding = 2 },
+				},
+				lualine_b = { "branch" },
+				lualine_c = {},
+				lualine_x = { "diagnostics", "filesize" },
+				lualine_y = { "filename" },
+				lualine_z = {
+					"location",
+					-- { "location", separator = { left = "", right = "" }, left_padding = 2 },
+				},
+			},
+			inactive_sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = {},
+				lualine_x = { "diagnostics", "filesize" },
+				lualine_y = { "filename" },
+				lualine_z = { "location" },
+			},
+			tabline = {},
+			extensions = {},
+		})
+	end,
 }
